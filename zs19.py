e1cfdba3e6e0dc6e85d98fb402cecd3fa622b68d
Zsolt Zombori <zombori@cs.bme.hu>
2023-04-06 16:30:20 +0200

    x

--- zs19.py
+++ zs19.py
@@ -2,23 +2,23 @@
 from run import run_all
 
 experiment=19
-gpus=[0,1,2,3,4,5,6]
-jobs_per_gpu=1
+gpus=[1,2,3,4,5,6]
+jobs_per_gpu=5
 outdir = "out/zs{}".format(experiment)
 
 args = {
     "-ds": ['cifar100'],
-    "-mo": ['resnet50'],
+    "-mo": ['resnet18'],
     "-lo": ['prp', 'nll'],
     "-lw": ['1'],
-    "-lr": ["0.005"],
+    "-lr": ["0.1"],
     "-wd": ["0.0005"],
-    "-bs": ["256"],
-    "-ep": ["100"],
+    "-bs": ["128"],
+    "-ep": ["200"],
     "-seed": ["0"],
     "-dseed": ["0"],
     "-cluster":["4"],
-    "-prt": ["0.0", "0.1", "0.8"],
+    "-prt": ["0.0", "0.05", "0.1", "0.3", "0.6"],
     "-num_groups":["1", "2", "4"],
     }
 
--- zs23.py
+++ zs23.py
@@ -1,25 +1,25 @@
 # effect of changing distance between centroids
 from run import run_all
 
-experiment=20
-gpus=[1,2,3,4,5,6]
-jobs_per_gpu=1
+experiment=23
+gpus=[0,1,2,3,4,5,6]
+jobs_per_gpu=5
 outdir = "out/zs{}".format(experiment)
 
 args = {
     "-ds": ['cifar100'],
     "-mo": ['resnet18'],
-    "-lo": ['prp', 'nll'],
+    "-lo": ['prp', 'nll', "prp_basic", "bi_prp", "democracy", "cc", "rc", "lws", "ll"],
     "-lw": ['1'],
-    "-lr": ["0.005", "0.01", "0.1"],
+    "-lr": ["0.01"],
     "-wd": ["0.0005"],
     "-bs": ["128"],
     "-ep": ["100"],
     "-seed": ["0"],
     "-dseed": ["0"],
     "-cluster":["4"],
-    "-prt": ["0.0"],
-    "-num_groups":["1"],
+    "-prt": ["0.0", "0.3", "0.6", "0.9"],
+    "-num_groups":["1", "2", "4", "5", "10", "20"],
     }
 
 print("Experiment", experiment)
